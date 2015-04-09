fa2ac1f27c1f7b0084f0d948dd8e733a13fd64e1
Marat <izummn@users.noreply.github.com>
2015-04-09 16:20:04 +0300

    Day_commit
    changes: not work! Need to copy stream.

--- bit_reader/bit_iterator/bit_iterator.hpp
+++ bit_reader/bit_iterator/bit_iterator.hpp
@@ -4,124 +4,60 @@
 #include <sstream>
 
 using namespace std;
-int bitCount = 0;
+int bitCount = 8;
 int byte[8] {0, 0, 0, 0, 0, 0, 0, 0};
 int flag = 0;
+
 class bit_iterator
 {
-	//std::ifstream thread;
+	std::ifstream s;
 	int charNumber;
 
-
-
 public:
-
-	bit_iterator(std::ifstream &stream)					//конструктор, в нем открывается файл. 
+	bit_iterator(std::ifstream &stream) 
 	{
-		char ch;
-		/*stream.get(ch);
-		charNumber = (int)ch;*/
-		
-		while (!stream.eof())
-		{
-			stream.get(ch);
-			
-			charNumber = (int)ch;
-			readBitFast();
-
-			cout << " flagg " << (int)ch;
-		}
-		// flag = -1;
-
-
-
+		s = stream;   //copy stream!
 	};
 
-	~bit_iterator()							// Деструктор. Нужен ли? тут корректно закрываем файл.
+	~bit_iterator()				
 	{
-	}
-
-
+	};
 
 	int readBit()
 	{
-		//cout << " flagg ";
 		if (flag == -1) return -1;
 
-		else 
-		{
-			bitCount++;
-			if (bitCount <= 7) return byte[bitCount];
-			else readBitFast();
-		};
-
-	}
-
-	
-	int readBitFast()						//здесь считывание через операцию & 0x1, вывод идет начиная со старших битов. Более производительна.
-	{
-		if (flag == -1) return -1;
 		else
+		{
+			
+			if (bitCount <= 7)
+			{
+				return byte[bitCount];
+				bitCount++;
+			}
+			else
+			{
+				readBitFast();
+				bitCount = 0;
+			};
+
+		};
+	};
+
+	void readBitFast()						
+	{
+		char ch;
+		while (!s.eof())
+		{
+			s.get(ch);
+			charNumber = abs((int)ch);
 			for (int j = 0; j < 8; ++j)
 			{
 				byte[j] = (charNumber & 0x1);
 				charNumber >>= 1;
 			}
+		}
+		flag = -1;
 	};
 
 };
-
-
-
-/*	int readBitFor()							//здесь происходит считывание через цикл for(;;), не могу проверить правильность.
-												//также я не могу проверить как происходит вывод: со старших или с младших бит? 
-	{												//(судя по реализации - обратный порядок тут не делал, поэтому как readBitFast(), со старших.)
-		int byte[8] = { 0, 0, 0, 0, 0, 0, 0, 0 }; 
-		int dec = readByte();
-		if (dec == -1) return -1;
-		else
-			for (int j = 7; j >= 0; j--)
-			{
-				byte[j] = dec % 2;
-				return byte[j];
-				dec >>= 1;
-			}
-	}
-
-	*/
-/*	int readBitBitset()									//вывод идет со старших битов.
-	{
-		int k = 0;
-		if (charNumber == -1) return -1;
-		else
-		{
-			std::bitset<8> byte(charNumber);
-			for (int i = 0; i < 8; i++)
-				return byte[i];
-		
-		};
-	}
-
-
-
-	
-
-	int readByte()							//функция, котороя достает конкретный символ.
-	{
-		char ch;
-		thread.exceptions(std::ifstream::badbit);
-		try {
-			while (!thread.eof())
-			{
-				thread.get(ch);
-				return abs((int)ch);
-			}
-			return -1;
-		}
-		catch (std::ifstream::failure &e)
-		{
-			std::cout << "Exception opening/reading/closing file\n";
-		}
-	};
-};
-*/
