7f8fe3a1770dbb6b1be6a85c441a10b1c46024ed
Marat <izummn@users.noreply.github.com>
2015-04-09 19:14:09 +0300

    through_string
    This is work programm, but through string

---
+++ bit_reader/bit_reader/bit_reader_through_string.cpp
@@ -0,0 +1,18 @@
+#include <C:\Users\NADERSHIN\Documents\Visual Studio 2013\Projects\one\bit_iterator\bit_iterator.hpp>;
+#include <iostream>
+
+int main()
+{
+	int cache, i;
+	std::ifstream f("1.txt", ios::binary);
+	bit_iterator b(f);						
+	int result;
+	do
+	{            
+		result = b.readBit();			
+		std::cout << result;
+	} while (result != -1);
+
+	std::cout << std::endl;
+	return 0;
+}
