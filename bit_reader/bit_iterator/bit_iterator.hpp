04a3f850e1e390508e3090ab0eddbe9aa6879054
Marat <izummn@users.noreply.github.com>
2015-04-10 17:25:20 +0300

    Day_commit
    Changes:
    Through fstream reference!

--- bit_reader/bit_iterator/bit_iterator.hpp
+++ bit_reader/bit_iterator/bit_iterator.hpp
@@ -2,62 +2,60 @@
 #include <bitset>
 #include <fstream>
 #include <sstream>
+#include <string>
 
 using namespace std;
-int bitCount = 8;
-int byte[8] {0, 0, 0, 0, 0, 0, 0, 0};
-int flag = 0;
 
 class bit_iterator
 {
-	std::ifstream s;
-	int charNumber;
+private:
+	std::ifstream &stream;
+	int bitCount = 8, countString = 1, flag = 0;
+	int byte[8];
 
 public:
-	bit_iterator(std::ifstream &stream) 
-	{
-		s = stream;   //copy stream!
-	};
-
-	~bit_iterator()				
-	{
-	};
-
+	bit_iterator(std::ifstream &p) :stream(p) {}
+	~bit_iterator(){}
 	int readBit()
 	{
 		if (flag == -1) return -1;
 
 		else
 		{
-			
-			if (bitCount <= 7)
+			if (bitCount <= 6)
 			{
-				return byte[bitCount];
 				bitCount++;
+				return byte[bitCount];
 			}
 			else
 			{
 				readBitFast();
 				bitCount = 0;
-			};
+				return byte[0];
+			}
 
-		};
-	};
+		}
+	}
 
-	void readBitFast()						
+
+	void readBitFast()
 	{
 		char ch;
-		while (!s.eof())
-		{
-			s.get(ch);
-			charNumber = abs((int)ch);
-			for (int j = 0; j < 8; ++j)
+		stream.seekg(countString, ios::beg);
+		stream.get(ch);
+
+		if (stream.eof()) flag = -1;
+		else
 			{
-				byte[j] = (charNumber & 0x1);
-				charNumber >>= 1;
-			}
-		}
-		flag = -1;
-	};
+				int charNumber = abs((int)ch);
+				for (int j = 0; j <= 7; ++j)
+				{
+					byte[j] = (charNumber & 0x1);
+					charNumber >>= 1;
+				}
+				countString++;
+		        }
+	
+	}
 
 };
