64fdd05e9bd3614927232d264a0b45da76e0d906
izummn <maratik1993@mail.ru>
2015-04-28 20:34:44 +0300

    bit_iterator with vector container
    Changes:
    1) change bit_iterator class
    2) change bit_reader - but not work

--- bit_reader/bit_iterator/bit_iterator.hpp
+++ bit_reader/bit_iterator/bit_iterator.hpp
@@ -1,18 +1,43 @@
 #include <iostream>
 #include <fstream>
 #include <array>
+#include <iterator>
+#include <vector>
 
-class bit_iterator
+class bit_iterator : public std::iterator <std::input_iterator_tag, int>
 {
 private:
-	std::ifstream &stream;
+	//std::ifstream &stream;
 	int bitCount = 8;
 	std::array <int, 8> byte;
+	std::vector<double>::iterator iter;
+	double* p;
+
 
 public:
+	//bit_iterator(std::ifstream &p) :stream(p) {}
 
-	bit_iterator(std::ifstream &p) :stream(p) {}
+	bit_iterator(std::vector<double>::iterator _iter) : iter(_iter) {}
+	bit_iterator(double* x) : p(x) {}
+
+	bit_iterator& operator++()
+	{
+		++p;
+		return *this;
+	}
+
+	bit_iterator operator++(int)
+	{
+		bit_iterator tmp(*this);
+		operator++();
+		return tmp;
+	}
+
+	bool operator==(const bit_iterator& rhs) { return p == rhs.p; }
+	bool operator!=(const bit_iterator& rhs) { return p != rhs.p; }
+	double& operator*() { return *p; }
 	~bit_iterator(){}
+
 
 	int readBit()
 	{
@@ -40,17 +65,20 @@ public:
 	void readBitFast()				//The conclusion from the lower bits
 	{
 		char ch;
-		stream.get(ch);
+		/*  stream.get(ch);
 		if (stream.eof()) byte[0] = -1;
 		else
 		{
-			int charNumber = (int)ch;
-			for (int j = 7; j >= 0; j--)
-			{				
-				byte[j] = (charNumber & 0x1);
-				charNumber >>= 1;
-			}
+		int charNumber = (int)ch;
+		for (int j = 7; j >= 0; j--)
+		{
+		byte[j] = (charNumber & 0x1);
+		charNumber >>= 1;
 		}
+		}*/
 	}
 
 };
+
+
+
--- bit_reader/bit_reader/bit_reader_code.cpp
+++ bit_reader/bit_reader/bit_reader_code.cpp
@@ -3,42 +3,21 @@
 #include <iostream>
 #include <bitset>
 #include <stdint.h>
+#include <vector>
 
 using namespace std;
 
 const int nBits = 32;
 
-
-int main(int argc, char *argv[])
+int main()
 {
-	if (argc < 2)
-	{
-		std::cout << "Enter file name!" << std::endl;
-		exit(1);
-	}
-	string name(argv[1]);
 
-
-	ifstream f(name, ios::binary);
-	f.exceptions(std::ifstream::badbit);
-	try {
-		bit_iterator b(f);
-		int result;
-		do
-		{
-			result = b.readBit();
-			if (result == -1) break;
-			cout << result;
-		} while (result != -1);
-		return 0;
-	}
-
-	catch (std::ifstream::failure &e)
-	{
-		std::cout << "Exception opening/reading/closing file\n";
-	}
-
-
-	
+	std::vector<double> v{ { 7, 8.1, 3.9, -48.6 } };
+	bit_iterator first = v.begin();
+	bit_iterator last = v.end();
+	std::copy(first, last, std::ostream_iterator<int>(std::cout, " "));
+	std::cout << std::endl;
+	return 0;
 
 }
+
